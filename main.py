140e2d7014dff04c9cf5cd200a59fde4e7e81fee
vlaskz <atrox.info@hotmail.com>
2021-07-17 21:00:06 -0300

    first Api Implementation FAP (For Academic Purposes). First working try.

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 # just trynna learn this faster(hahaha) as I can.
 # Author: Isaias Velasquez.
 
-import server as s
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"hello": "world"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 if __name__ == '__main__':
-    s.run()
+    uvicorn.run(app)
